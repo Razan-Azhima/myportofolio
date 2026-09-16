43e91541463d70e6c6de02a6db499a5c9a48a846
Razan-Azhima <razan.alif@ui.ac.id>
2026-09-16 17:14:23 +0700

    fixed a bug where the db gets all confused

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from main.views import show_main, show_experience, show_skills, show_achievements, show_education
+from main.views import show_main, show_experience, show_skills, show_achievements, show_education, create_project
 
 app_name = "main"
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("skills/", show_skills, name="show_skills"),
     path("achievements/", show_achievements, name="show_achievements"),
     path("education/", show_education, name="show_education"),
+    path("projects/add/", create_project, name="create_project"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from main.models import Experience
+from main.models import Experience, Project
+
+from django.contrib import messages
+from django.core import serializers
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from main.forms import ProjectForm
 
 # Create your views here.
 def show_main(request):
@@ -48,3 +54,24 @@ def show_skills(request):
         'skill_list': skill_list,
     }
     return render(request, 'skills.html', context)
+
+def create_project(request):
+    form = ProjectForm(request.POST or None)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        messages.success(request, "Proyek baru berhasil ditambahkan!")
+        return redirect("main:show_projects")
+
+    context = {
+        "name": "Razan Alif Azhima",
+        "form": form,
+    }
+    return render(request, "projects_form.html", context)
+
+def show_projects(request):
+    context = {
+        "name": "Razan Alif Azhima",
+        "project_list": Project.objects.all(),
+    }
+    return render(request, "project.html", context)
